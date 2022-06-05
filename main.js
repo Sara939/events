070656547b921bf5838b2a3eb4860419f7d8807c
sara-likaount <saralikaunt@gmail.com>
2022-06-05 22:40:45 +0300

    s

---
+++ main.js
@@ -0,0 +1,32 @@
+// 1:
+// function alertmsg(){
+//     alert("welcom");
+// }
+// 2:
+// function timefunc(){
+//     var time= new Date();
+//     console.log(time);
+// }
+// 3:
+// function colorchng(){
+//     var mytag= document.getElementById("myh3");
+//     mytag.style.color = "red";
+// 4:
+// function widthchng(){
+//     var mine= document.getElementById("mybutton");
+//     mine.style.width= "500px";
+
+// }
+// 5:
+// function chng(){
+//     var othertext= document.getElementById("mytext");
+//     othertext.textContent = "this new text";
+// }
+// 6:///////////////////////////////////////////////////////////////////
+// function getname(){
+//     var usern= prompt("enter name");
+//     console.log(usern);
+// }
+// function print(){
+//     document.write(usern);
+// }
