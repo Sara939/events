2d986411b88b6d2657147f7289e9055390561daa
sara-likaount <saralikaunt@gmail.com>
2022-06-12 22:42:19 +0300

    n

--- main.js
+++ main.js
@@ -1,117 +1,124 @@
-// 1:
-// function alertmsg(){
-//     alert("welcom");
-// }
-// 2:
-// function timefunc(){
-//     var time= new Date();
-//     console.log(time);
-// }
-// 3:
-// function colorchng(){
-//     var mytag= document.getElementById("myh3");
-//     mytag.style.color = "red";
-// 4:
-// function widthchng(){
-//     var mine= document.getElementById("mybutton");
-//     mine.style.width= "500px";
+// // 1:
+// // function alertmsg(){
+// //     alert("welcom");
+// // }
+// // 2:
+// // function timefunc(){
+// //     var time= new Date();
+// //     console.log(time);
+// // }
+// // 3:
+// // function colorchng(){
+// //     var mytag= document.getElementById("myh3");
+// //     mytag.style.color = "red";
+// // 4:
+// // function widthchng(){
+// //     var mine= document.getElementById("mybutton");
+// //     mine.style.width= "500px";
 
-// }
-// 5:
-// function chng(){
-//     var othertext= document.getElementById("mytext");
-//     othertext.textContent = "this new text";
-// }
-// 6:
+// // }
+// // 5:
+// // function chng(){
+// //     var othertext= document.getElementById("mytext");
+// //     othertext.textContent = "this new text";
+// // }
+// // 6:
 
-// function getname(){
-//     var username= prompt("enter your name");
-//     document.write(username);
-// }
-// function print(){
-//     var whenpreesed= document.getElementById("click");
-//     getname();
+// // function getname(){
+// //     var username= prompt("enter your name");
+// //     document.write(username);
+// // }
+// // function print(){
+// //     var whenpreesed= document.getElementById("click");
+// //     getname();
     
-// }
-// 7:
-//     var myobje= document.getElementById("mydate");
-//     var currenttime= new Date().getHours();
-// window.onload= function windcolor(){
-//     if (currenttime < 17){
-//         myobje.style.backgroundColor = "yellow";
+// // }
+// // 7:
+// //     var myobje= document.getElementById("mydate");
+// //     var currenttime= new Date().getHours();
+// // window.onload= function windcolor(){
+// //     if (currenttime < 17){
+// //         myobje.style.backgroundColor = "yellow";
 
-//     }
-//     else{
-//         myobje.style.backgroundColor = "blue";
-// }
-// windgreeding();
-// }
+// //     }
+// //     else{
+// //         myobje.style.backgroundColor = "blue";
+// // }
+// // windgreeding();
+// // }
 
-///////////////////////////////////////////////////////////////
-// function windgreeding(){
-//     var greed= document.getElementById("mydate");
-//     if (currenttime < 12){
-//         greed.innerHTML = "good day";
-//         document.write(greed);
+// ///////////////////////////////////////////////////////////////
+// // function windgreeding(){
+// //     var greed= document.getElementById("mydate");
+// //     if (currenttime < 12){
+// //         greed.innerHTML = "good day";
+// //         document.write(greed);
 
-//     }
-//     else{
-//         greed.innerHTML =" good evening";
-//         document.write(greed);
-//     }
+// //     }
+// //     else{
+// //         greed.innerHTML =" good evening";
+// //         document.write(greed);
+// //     }
 
-// }
-// 8:///////////////////////////////////////////////////////////////////////////////////////
-// var userage= +prompt("enter your age");
+// // }
+// // 8:///////////////////////////////////////////////////////////////////////////////////////
+// // var userage= +prompt("enter your age");
 
-// function startage(){
-//     document.write("welcom");
-// }
-// msgred= document.getElementById("mydiv").addEventListener("load",getage);
+// // function startage(){
+// //     document.write("welcom");
+// // }
+// // msgred= document.getElementById("mydiv").addEventListener("load",getage);
 
-//  function getage(){
-//     if(userage>18){
-//         msgred=document.getElementById("mydiv");
-//         msgred.style.color = "red";
-//     }
-//     else{  
-//         msgred.style.color = "green";
-//     }
+// //  function getage(){
+// //     if(userage>18){
+// //         msgred=document.getElementById("mydiv");
+// //         msgred.style.color = "red";
+// //     }
+// //     else{  
+// //         msgred.style.color = "green";
+// //     }
     
-// }
+// // }
 
-// msgred= document.getElementById("mydiv").addEventListener("mouseover",chgcolor);
-// function chgcolor(){
+// // msgred= document.getElementById("mydiv").addEventListener("mouseover",chgcolor);
+// // function chgcolor(){
    
-//     msgred.style.color = "green";
+// //     msgred.style.color = "green";
+// // }
+
+// // 9://////////////////////////////////////////////////////////////////////////////
+
+// // function myinput(){
+// //     if(){
+// //     var usersinput = document.getElementById("myInput");
+// //     document.write(usersinput.value);
+// //     }
+
+// // }
+
+// function rannum(){
+//     var mynum=[];
+//     for(var i= 0; i<5; i++){
+//        mynum= Math.floor(Math.random() * 57);
+//        document.getElementById("myh1").innerText+= mynum;
+//     }
+    
 // }
 
-// 9:
-function myinput(){
-    for(var i=0; i<6; i++){
-    var usersinput = document.getElementById("myInput").onclick= ()=>{
-        document.write(usersinput);   
-    }
-    
-}
-}
-function rannum(){
-    for(var i= 0; i<5; i++){
-       var mynum= Math.floor(Math.random()*57);
-       document.write(mynum); 
-    }
-    
-}
+// // function mylotu(){
+// //     myinput();
+// //     rannum();
+// //     if(mynum[i]==usersinput[i]){
+// //         document.write("sucsess"); 
+// //     }
+// //     else{
+// //         document.write("fail");
+// //     }
+// // }
 
-function mylotu(){
-    myinput();
-    rannum();
-    if(mynum[i]==usersinput[i]){
-        document.write("sucsess"); 
-    }
-    else{
-        document.write("fail");
-    }
-}
+// // mylotu();
 
-mylotu();
+
+// 2/6:
+
+
